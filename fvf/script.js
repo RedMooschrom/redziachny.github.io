70a3fc1f05561edc9ce16154e348719ba39ea309
RedMooschrom <kubaoxx@gmail.com>
2024-07-03 22:57:25 +0200

    :)

--- fvf/script.js
+++ fvf/script.js
@@ -24,7 +24,11 @@ document.addEventListener('DOMContentLoaded', function() {
                         const killsMatch = latestUserData.match(/"totalKills":(\d+),/);
                         const deathsMatch = latestUserData.match(/"totalDeaths":(\d+),/);
                         const ragequitsMatch = latestUserData.match(/"ragequits":(\d+),/);
-                        const nicknameMatch = latestUserData.match(/"nickname":"([^"]+)",/); // New line to match nickname
+                        const nicknameMatch = latestUserData.match(/"nickname":"([^"]+)",/); 
+                        const userId = latestUserData.match(/"userId":"([^"]+)",/);
+                        const friendCode = latestUserData.match(/"friendCode":"([^"]+)",/);
+                        const eloMatch = latestUserData.match(/"elo":(\d+)/);
+                        const rankIdMatch = latestUserData.match(/"rankId":(\d+)/);
 
                         // Parse additional variables
                         const lifetimeGemsSpentMatch = latestUserData.match(/"lifetimeGemsSpent":(\d+),/);
@@ -42,12 +46,24 @@ document.addEventListener('DOMContentLoaded', function() {
                         const lifetimeVictoriesMatch = latestUserData.match(/"lifetimeVictories":(\d+),/);
                         const lifetimeDefeatsMatch = latestUserData.match(/"lifetimeDefeats":(\d+),/);
                         const lifetimeTiesMatch = latestUserData.match(/"lifetimeTies":(\d+),/);
+                        const bansMatch = latestUserData.match(/"softBan":\{"liftDate":"[^"]+","count":(\d+),/);
+
+                        const createdAtMatch = latestUserData.match(/"createdAt":"([^"]+)"/);
+                let formattedCreatedAt = '';
+                if (createdAtMatch) {
+                    const createdAtDate = new Date(createdAtMatch[1]);
+                    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+                    formattedCreatedAt = createdAtDate.toLocaleDateString('en-US', options);
+                }
 
                         return {
                             totalKills: killsMatch ? parseInt(killsMatch[1]) : 0,
                             totalDeaths: deathsMatch ? parseInt(deathsMatch[1]) : 0,
                             ragequits: ragequitsMatch ? parseInt(ragequitsMatch[1]) : 0,
-                            nickname: nicknameMatch ? nicknameMatch[1] : '', // Include nickname in the return object
+                            nickname: nicknameMatch ? nicknameMatch[1] : '', 
+                            userId: userId ? userId[1] : '', 
+                            friendCode: friendCode ? friendCode[1] : '', 
+                            createdAt: formattedCreatedAt,
                             lifetimeGemsSpent: lifetimeGemsSpentMatch ? parseInt(lifetimeGemsSpentMatch[1]) : 0,
                             lifetimeMoneySpent: lifetimeMoneySpentMatch ? parseInt(lifetimeMoneySpentMatch[1]) : 0,
                             lifetimeGemsEarned: lifetimeGemsEarnedMatch ? parseInt(lifetimeGemsEarnedMatch[1]) : 0,
@@ -60,6 +76,9 @@ document.addEventListener('DOMContentLoaded', function() {
                             lifetimeVictories: lifetimeVictoriesMatch ? parseInt(lifetimeVictoriesMatch[1]) : 0,
                             lifetimeDefeats: lifetimeDefeatsMatch ? parseInt(lifetimeDefeatsMatch[1]) : 0,
                             lifetimeTies: lifetimeTiesMatch ? parseInt(lifetimeTiesMatch[1]) : 0,
+                            bans: bansMatch ? parseInt(bansMatch[1]) : 0,
+                            elo: eloMatch ? parseInt(eloMatch[1]) : 0,
+                            rankId: rankIdMatch ? parseInt(rankIdMatch[1]) : 0,
                             latestUserData: latestUserData // Include the latest user data section for further parsing
                         };
                     }
@@ -73,7 +92,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     "DINGO": "Dingo",
                     "MOOSE": "Moose Salto",
                     "SEAGULL": "Stevie Gull",
-                    "DUCK": "Duck Anderson ",
+                    "DUCK": "Duck Anderson",
                     "CROCODILE": "DJ Newton",
                     "TIGER": "Sable Santana",
                     "DOBERMAN": "Donnie B.",
@@ -82,10 +101,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     "RAT": "Klustr Jr.",
                     "WOODPECKER": "Myk",
                     "HARE": "Haru",
-                    "WOLF": "Dane Donovan"
+                    "WOLF": "Dale Donovan"
                 };
 
-                const values = {};
 
                 // Parse latest user data
                 const latestUserData = parseLatestUserData(text);
@@ -175,7 +193,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         tile.classList.add('character-tile');
 
                         tile.innerHTML = `
-                            <p id="character_name">${tileData.character}<p>
+                            <p id="character_name">${tileData.character} <img style="width: 48px; height: 48px;" src="characters/${tileData.character}.png" /></p>
                             <p>Total Matches: ${tileData.matches}</p>
                             <p>Wins: ${tileData.victories}</p>
                             <p>Losses: ${tileData.defeats}</p>
@@ -186,34 +204,123 @@ document.addEventListener('DOMContentLoaded', function() {
                         characterContainer.appendChild(tile);
                     });
 
-                    // Update overall statistics after sorting
-                    const overallTile = document.createElement('div');
+                    // Formatting function for packs opened per type
+                    function formatPacksOpened(packs) {
+                        return packs.map(pack => {
+                            let packDescription;
+                            switch (pack.id) {
+                                case 1:
+                                    packDescription = `Basic: ${pack.timesUsed}`;
+                                    break;
+                                case 2:
+                                    packDescription = `Golden: ${pack.timesUsed}`;
+                                    break;
+                                case 3:
+                                    packDescription = `Epic: ${pack.timesUsed}`;
+                                    break;
+                                case 4:
+                                    packDescription =`Legendary: ${pack.timesUsed}`;
+                                    break;
+                                case 7:
+                                    packDescription = `Collectors: ${pack.timesUsed}`;
+                                    break;
+                                case 100:
+                                    const percentage = (pack.timesUsed / 99 * 100).toFixed(2);
+                                    packDescription = `Shiny: ${pack.timesUsed}/99 (${percentage}%)`;
+                                    break;
+                                default:
+                                    packDescription = `Unknown Pack ID ${pack.id}`;
+                            }
+                            return `${packDescription}`;
+                        }).join('<br>');
+                    }
+
+                    // Function to format packs bought per type
+function formatPacksBought(packs) {
+    return packs.map(pack => {
+        let packDescription;
+        switch (pack.id) {
+            case 1:
+                packDescription = `Basic: ${pack.timesUsed}`;
+                break;
+            case 2:
+                packDescription = `Golden: ${pack.timesUsed}`;
+                break;
+            default:
+                packDescription = `Unknown Pack ID ${pack.id}`;
+        }
+        return `${packDescription}`;
+    }).join('<br>');
+}
+
+function formatGachasOpened(gachas) {
+    let totalNewGachas = 0;
+    let totalOldGachas = 0;
+
+    const gachaDescriptions = gachas.map(gacha => {
+        let gachaDescription;
+        switch (gacha.id) {
+            case 1:
+                gachaDescription = `New Gachas: ${gacha.timesUsed}`;
+                totalNewGachas += gacha.timesUsed;
+                break;
+            case 2:
+                gachaDescription = `Old Gachas: ${gacha.timesUsed}`;
+                totalOldGachas += gacha.timesUsed;
+                break;
+            default:
+                gachaDescription = `Unknown Gacha ID ${gacha.id}`;
+        }
+        return `${gachaDescription}`;
+    });
+
+    const totalGachas = totalNewGachas + totalOldGachas;
+    const percentage = ((totalGachas / 197) * 100).toFixed(2);
+    const summary = `Altogether: ${totalGachas}/197 (${percentage}%)`;
+
+    gachaDescriptions.push(summary);
+
+    return gachaDescriptions.join('<br>');
+}
+                    const overallTile = document.getElementById('overallTile');
+                    overallTile.innerHTML = ''; // Clear previous content
                     overallTile.classList.add('overall-tile');
 
                     overallTile.innerHTML = `
-                        <p id="overall_stats">Overall Statistics<p>
-                        <p>Nickname: ${latestUserData.nickname}</p>
-                        <p>Total Matches: ${totalMatches}</p>
-                        <p>Total Wins: ${totalVictories}</p>
-                        <p>Total Losses: ${totalDefeats}</p>
-                        <p>Total Ties: ${totalTies}</p>
-                        <p>Overall Win Ratio: ${(totalVictories / totalMatches * 100).toFixed(2)}%</p>
-                        ${latestUserData ? `
-                        <p>Total Kills: ${latestUserData.totalKills}</p>
-                        <p>Total Deaths: ${latestUserData.totalDeaths}</p>
-                        <p>Ragequits: ${latestUserData.ragequits}</p>
-                        <p>Money Earned: ${latestUserData.lifetimeMoneyEarned}</p>
-                        <p>Money Spent: ${latestUserData.lifetimeMoneySpent}</p>
-                        <p>Gems Earned: ${latestUserData.lifetimeGemsEarned}</p>
-                        <p>Gems Spent: ${latestUserData.lifetimeGemsSpent}</p>
-                        <p>Gachas Opened: ${latestUserData.lifetimeGachasOpened}</p>
-                        <p>Packs Bought Per Type: ${JSON.stringify(latestUserData.packsBoughtPerType)}</p>
-                        <p>Packs Opened Per Type: ${JSON.stringify(latestUserData.packsOpenedPerType)}</p>
-                        <p>Gachas Opened Per Type: ${JSON.stringify(latestUserData.gachasOpenedPerType)}</p>
-                        ` : ''}
-                    `;
+                    <p class="overall_stats_titles" style="font-size: 40px;">User Details:</p>
+                    <p>Nickname: ${latestUserData.nickname}</p>
+                    <p>ELO: ${latestUserData.elo}/10000 <img style="width: 32px; height: 32px;" src="ranks/${latestUserData.rankId}.png"></img></p>
+                    <p>User ID: ${latestUserData.userId}</p>
+                    <p>Friend Code: ${latestUserData.friendCode}</p>
+                    <p>Friend Since: ${latestUserData.createdAt}</p>
+                    <p>Bans: ${latestUserData.bans}</p>
+                    <p class="overall_stats_titles" style="font-size: 40px;">Total Match Stats:</p>
+                    <p>Matches Played: ${totalMatches}</p>
+                    <p>Wins: ${totalVictories}</p>
+                    <p>Losses: ${totalDefeats}</p>
+                    <p>Ties: ${totalTies}</p>
+                    <p>Ragequits: ${latestUserData.ragequits}</p>
+                    <p>Win Ratio: ${(totalVictories / totalMatches * 100).toFixed(2)}%</p>
+                    ${latestUserData ? `
+                    <p class="overall_stats_titles" style="font-size: 40px;">K/D Stats:</p>
+                    <p>Kills: ${latestUserData.totalKills}</p>
+                    <p>Deaths: ${latestUserData.totalDeaths}</p>
+                    <p> KD Ratio: ${(latestUserData.totalKills / latestUserData.totalDeaths).toFixed(2)}</p>
+                    <p class="overall_stats_titles" style="font-size: 40px;">Wealth Stats:</p>
+                    <p>Money Earned: ${latestUserData.lifetimeMoneyEarned}</p>
+                    <p>Money Spent: ${latestUserData.lifetimeMoneySpent}</p>
+                    <p>Gems Earned: ${latestUserData.lifetimeGemsEarned}</p>
+                    <p>Gems Spent: ${latestUserData.lifetimeGemsSpent}</p>
+                    <p class="overall_stats_titles" style="font-size: 40px;">Packs Bought:</p>
+                    <p>${formatPacksBought(latestUserData.packsBoughtPerType)}</p>
+                    <p class="overall_stats_titles" style="font-size: 40px;">Packs Opened:</p>
+                    <p>${formatPacksOpened(latestUserData.packsOpenedPerType)}</p>
+                    <p class="overall_stats_titles" style="font-size: 40px;">Gachas Opened:</p>
+                    <p>${formatGachasOpened(latestUserData.gachasOpenedPerType)}</p>
+                    ` : ''}
+                `;
+       
 
-                    characterContainer.appendChild(overallTile);
                 }
 
                 // Sort by default (e.g., most wins)
