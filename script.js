38933354e99b1fcd8d0e80ab3e6163e6e1e25e9b
RedMooschrom <kubaoxx@gmail.com>
2024-07-13 23:38:06 +0200

    Cards stats

--- script.js
+++ script.js
@@ -47,6 +47,9 @@ document.addEventListener('DOMContentLoaded', function() {
                         const lifetimeDefeatsMatch = latestUserData.match(/"lifetimeDefeats":(\d+),/);
                         const lifetimeTiesMatch = latestUserData.match(/"lifetimeTies":(\d+),/);
                         const bansMatch = latestUserData.match(/"softBan":\{"liftDate":"[^"]+","count":(\d+),/);
+                        const cardsUsedMatch = latestUserData.match(/"cardsUsed":(\[.*?\])/);
+
+
 
                         const createdAtMatch = latestUserData.match(/"createdAt":"([^"]+)"/);
                 let formattedCreatedAt = '';
@@ -79,7 +82,8 @@ document.addEventListener('DOMContentLoaded', function() {
                             bans: bansMatch ? parseInt(bansMatch[1]) : 0,
                             elo: eloMatch ? parseInt(eloMatch[1]) : 0,
                             rankId: rankIdMatch ? parseInt(rankIdMatch[1]) : 0,
-                            latestUserData: latestUserData // Include the latest user data section for further parsing
+                            cardsUsed: cardsUsedMatch ? JSON.parse(cardsUsedMatch[1]) : [],
+                            latestUserData: latestUserData, // Include the latest user data section for further parsing
                         };
                     }
 
@@ -105,8 +109,225 @@ document.addEventListener('DOMContentLoaded', function() {
                 };
 
 
+                const ignoredCardIds = [167]; // Add the IDs you want to ignore here
+
                 // Parse latest user data
                 const latestUserData = parseLatestUserData(text);
+
+                const cardMap = {
+                    1: "Big Head",
+                    2: "Small Head",
+                    3: "Health Down",
+                    4: "Health Up",
+                    5: "Move Slower",
+                    6: "Move Faster",
+                    7: "Double Jump",
+                    8: "No Jump",
+                    9: "Boomstick",
+                    10: "Albatross 21",
+                    11: "Less Accuracy",
+                    12: "More Accuracy",
+                    13: "Rubber Bullets",
+                    14: "Steel Bullets",
+                    15: "Small Mag",
+                    16: "Big Mag",
+                    17: "Slow Reload",
+                    18: "Quick Reload",
+                    19: "Silent Steps",
+                    20: "Brasshopper",
+                    21: "Laika",
+                    22: "Medkit",
+                    23: "Invisible Health",
+                    24: "Invisible Hand",
+                    25: "FK-82",
+                    26: "Bullet Time",
+                    27: "Poison",
+                    28: "Vampire Bullets",
+                    29: "Curse",
+                    30: "Energy Drink",
+                    31: "Helmet",
+                    32: "Akimbo",
+                    33: "Ca-Turret",
+                    34: "Titan",
+                    35: "Disarm",
+                    36: "Poison Bullets",
+                    37: "Bomb",
+                    38: "Tin Man",
+                    39: "Triple Jump",
+                    40: "Cancel Titan",
+                    41: "Wall",
+                    42: "Bomb Belt",
+                    43: "Bomb Belt's Bomb",
+                    44: "Garbage Day",
+                    45: "Garbage",
+                    46: "Ice Block",
+                    47: "Bouncy Wall",
+                    48: "Smoke Bomb",
+                    49: "Flash Bomb",
+                    50: "Extra Cards",
+                    51: "Card Profaner",
+                    52: "Rock",
+                    53: "Paper",
+                    54: "Scissors",
+                    55: "Rock, Paper, Scissors",
+                    56: "Card Thief",
+                    57: "Land Mine",
+                    58: "Bear Trap",
+                    59: "Invisible",
+                    60: "Mind Blowing",
+                    61: "Heartless",
+                    62: "Nuke",
+                    63: "Golden Boira",
+                    64: "Sticky Bomb",
+                    65: "Green Herb (Unused)",
+                    66: "Green Herb",
+                    67: "Punch-R",
+                    68: "Toxic Bomb",
+                    69: "Katana",
+                    70: "Swap Weapons",
+                    71: "Predator Vision",
+                    72: "Phantom Bullets",
+                    73: "Invisible Ammo",
+                    74: "Clown Shoes",
+                    75: "Empty Mag",
+                    76: "Backache",
+                    77: "Armored Vest",
+                    78: "Lottery Ticket",
+                    79: "Hand Trap",
+                    80: "Hand Trap!",
+                    81: "Hand Bomb",
+                    82: "Hand Bomb!",
+                    83: "Hot Potato",
+                    84: "Hot Potato!",
+                    85: "Bomb Lover",
+                    86: "Frozen Gun",
+                    87: "First Aid Training",
+                    88: "Kaboomber",
+                    89: "Ricochets!",
+                    90: "Counter Card",
+                    91: "Mirror Card",
+                    92: "Love Letter",
+                    93: "Love Letter",
+                    94: "Ninja Smoke",
+                    95: "Watered Explosions",
+                    96: "Bigger Explosions",
+                    97: "Gun Stealer",
+                    98: "Thick Skin",
+                    99: "Berserker",
+                    100: "Ninja Log",
+                    101: "Itchy Feet",
+                    102: "Self-Destruct Device",
+                    103: "Self-Destruct",
+                    104: "Karrotov",
+                    105: "Toxic Fungus",
+                    106: "Toxic Fungus",
+                    107: "Fungi Family",
+                    108: "Casa!",
+                    109: "Parley",
+                    110: "Butter Hands",
+                    111: "Barbed Cards",
+                    112: "Tramper",
+                    113: "Painkillers",
+                    114: "Pixel-Vision",
+                    115: "F",
+                    116: "R",
+                    117: "I",
+                    118: "E",
+                    119: "N",
+                    120: "D",
+                    121: "S",
+                    122: "High Stakes",
+                    123: "Invisible Cards",
+                    124: "Dr. Molebot",
+                    125: "#!#CA_ALARM_DRONE_NAME#!#",
+                    126: "#!#CA_GUNNER_DRONE_NAME#!#",
+                    127: "Pyromania",
+                    128: "Warp Room",
+                    129: "#!#CA_HOLD_DECOY_NAME#!#",
+                    130: "#!#CA_IRON_ARMS_NAME#!#",
+                    131: "Deep Fryer",
+                    132: "#!#CA_JETPACK_NAME#!#",
+                    133: "#!#CA_DASH_THRUSTER_NAME#!#",
+                    134: "#!#CA_ELECTRIC_BULLETS_NAME#!#",
+                    135: "#!#CA_ELECTRIC_HOOK_NAME#!#",
+                    136: "#!#CA_ELECTRIC_FENCE_NAME#!#",
+                    137: "Teleport Bomb",
+                    138: "Miniturret",
+                    139: "Shrink Spell",
+                    140: "Laser Fence",
+                    141: "Laser Fence Pole",
+                    142: "Jelly Bullets",
+                    143: "Bat Turret Lover",
+                    144: "Bat Turret",
+                    145: "Dither",
+                    146: "Banding",
+                    147: "Silence!",
+                    148: "Reroll",
+                    149: "Dice Bomb",
+                    150: "#!#CA_D20_NAME#!#",
+                    151: "Regeneration",
+                    152: "Present",
+                    153: "Predator Vision (Unused)",
+                    154: "Phantom Bullets (Unused)",
+                    155: "Floor is Lava",
+                    156: "#!#CA_DETECTIVE_NAME#!#",
+                    157: "Venom Eater",
+                    158: "Margarita Green Arpeggio",
+                    159: "Margarita Red Arpeggio",
+                    160: "Margarita Yellow Arpeggio",
+                    161: "Margarita Purple Arpeggio",
+                    162: "Margarita Pink Arpeggio",
+                    163: "Margarita Blue Arpeggio",
+                    164: "Brain Mirror",
+                    165: "Sly Shooter",
+                    166: "404",
+                    1001: "It's Medicinal",
+                    1002: "Brasslover",
+                    1003: "Reading Glasses",
+                    1004: "Badass",
+                    1005: "Classic",
+                    1006: "Thick Coat",
+                    1007: "Power Legs",
+                    1008: "Ammo Maniac",
+                    1009: "The More, The Better",
+                    1010: "Runner",
+                    1011: "Katana Lover",
+                    1012: "Demo Man",
+                    1013: "Big Bullets",
+                    1014: "Dither & Banding",
+                    1015: "Invisible",
+                    1016: "#!#CA_DUNGEON_MASTER_NAME#!#",
+                    1017: "Roleplay",
+                    1018: "#!#CA_HEALTHY_NAME#!#",
+                    1019: "Arpeggio",
+                    1020: "Venom Eater (Unused)",
+                    1021: "Undercover Agent",
+                    1022: "Left Behind",
+                    1023: "At the end of the Road",
+                    1024: "Venom Eater",
+                    1025: "Fluffy Wool",
+                    2000: "#!#CA_HANDICAP_NAME#!#",
+                    2001: "Small Head (Perk)",
+                    2002: "Health Up (Perk)",
+                    2003: "Move Faster (Perk)",
+                    2004: "More Accuracy (Perk)",
+                    2005: "Big Mag (Perk)",
+                    2006: "Quick Reload (Perk)",
+                    2007: "Thick Skin (Perk)",
+                    2008: "Steel Bullets (Perk)",
+                    2009: "Vampire Bullets (Perk)",
+                    2010: "Poison Bullets (Perk)",
+                    2011: "Phantom Bullets (Perk)",
+                    2012: "Silent Steps (Perk)",
+                    2013: "Bomb Belt (Perk)",
+                    2014: "Bat Turret Lover (Perk)",
+                    2015: "Bigger Explosions (Perk)",
+                    2016: "Double Jump (Perk)",
+                    2017: "Steel Bullets (Unused Perk)",
+                    2100: "#!#CA_HANDICAP_NAME#!#",
+                };
+
+                
 
                 // Container for character tiles
                 const characterContainer = document.getElementById('characterContainer');
@@ -155,6 +376,74 @@ document.addEventListener('DOMContentLoaded', function() {
                     });
                 }
 
+                const cardContainer = document.getElementById('cardContainer');
+                cardContainer.innerHTML = ''; // Clear previous content
+
+// Parse cards used
+if (latestUserData && latestUserData.cardsUsed) {
+    const cardsUsed = latestUserData.cardsUsed;
+    cardTiles = [];
+
+    cardsUsed.forEach(card => {
+        if (!ignoredCardIds.includes(card.id)) {
+            const tile = {
+                cardName: cardMap[card.id] || `Unknown Card ID ${card.id}`,
+                timesUsed: card.timesUsed
+            };
+
+            cardTiles.push(tile);
+        }
+    });
+
+     totalCardsUsed = cardsUsed.reduce((sum, card) => {
+        return ignoredCardIds.includes(card.id) ? sum : sum + card.timesUsed;
+    }, 0);
+}
+                // Sorting function for card tiles
+function sortCardTiles(criteria) {
+    cardTiles.sort((a, b) => {
+        switch (criteria) {
+            case 'mostUsed':
+                return b.timesUsed - a.timesUsed;
+            case 'leastUsed':
+                return a.timesUsed - b.timesUsed;
+            default:
+                return 0;
+        }
+    });
+
+    document.getElementById('cardSortCriteria').addEventListener('change', function() {
+        const criteria = this.value;
+        sortCardTiles(criteria);
+    });
+
+    // Clear container
+    cardContainer.innerHTML = '';
+
+
+    // Re-append sorted tiles
+    cardTiles.forEach(tileData => {
+        const cardTile = document.createElement('div');
+        cardTile.classList.add('character-tile');
+
+        const percentage = ((tileData.timesUsed / totalCardsUsed) * 100).toFixed(3);
+
+        document.getElementById("cardsBackground").style.display = "flex";
+
+        cardTile.innerHTML = `
+            <p id="card_name">${tileData.cardName}</p>
+            <p>Times Used: ${tileData.timesUsed}</p>
+            <p>Usage: ${percentage}%</p>
+        `;
+
+        cardContainer.appendChild(cardTile);
+    });
+}
+
+// Example of calling the sorting function
+sortCardTiles('mostUsed'); // Sort by most used cards
+
+// Call this function after parsing the data
                 // Sorting function
                 function sortTiles(criteria) {
                     characterTiles.sort((a, b) => {
@@ -319,7 +608,7 @@ function formatGachasOpened(gachas) {
                     <p>${formatGachasOpened(latestUserData.gachasOpenedPerType)}</p>
                     ` : ''}
                 `;
-       
+                
 
                 }
 
